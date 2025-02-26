17935a10e6b75a6392b8601c1c39edb66b513cbd
Siva <sivabalan.r@pionglobal.com>
2025-02-26 13:39:32 +0530

    portfloio 26a

---
+++ src/components/GmailCopy.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const GmailCopy = () => {
+  const gmailAddress = "sivabalanrajendran21@gmail.com";
+
+  const copyToClipboard = () => {
+    navigator.clipboard
+      .writeText(gmailAddress)
+      .then(() => {
+        alert("Gmail address copied to clipboard!");
+      })
+      .catch((err) => {
+        console.error("Failed to copy:", err);
+      });
+  };
+
+  return <div onClick={copyToClipboard}>{gmailAddress}</div>;
+};
+
+export default GmailCopy;
